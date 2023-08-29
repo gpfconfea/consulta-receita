3ce4da7d56baae39c8818bdfa193bc07e144b7d1
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2023-08-29 16:56:19 -0300

    updates on checkUpdate function

--- app/modules/checkUpdate.py
+++ app/modules/checkUpdate.py
@@ -1,3 +1,11 @@
+"""
+O código presente não é a versão final da função.
+ utilizaremos a API de extração de dados para gerar
+ a data da última extração, que será comparada com
+ a data de atualização do site da receita federal.
+ """
+
+
 from datetime import datetime
 from bs4 import BeautifulSoup
 import requests
@@ -8,13 +16,5 @@ def checkUpdate():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     last_update = soup.select('td')[7].text
-    last_update = datetime.strptime(last_update, "%Y-%m-%d %H:%M ").date()
-
-    if last_update < datetime.now().date():
-        print('Os dados estão atualizados!')
-        print(f'Última atualização: {last_update}')
-    else:
-        print('Atualização necessária!')
-        print(f'Data de atualização: {last_update}')
-
-checkUpdate()
+    last_update = datetime.strptime(last_update, "%Y-%m-%d %H:%M ")
+    print(f'Última atualização de dados no site: {last_update}')
