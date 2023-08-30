7ed78428a44aab64ab42f8cd0cdcf7fcfd38cf10
Felipe de Sousa <felipescoelho517@gmail.com>
2023-08-30 16:26:23 -0300

    feat: add do fork dos downloads da receita

---
+++ app/cnpj-sqlite/main.py
@@ -0,0 +1,9 @@
+from dados_cnpj_baixa import *
+from dados_cnpj_cnae_secundaria import *
+from dados_cnpj_lista_url import *
+from dados_cnpj_para_sqlite import *
+
+lista_url()
+baixa()
+sqlite()
+cnae_sec()
