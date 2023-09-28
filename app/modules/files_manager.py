20ed0723a54292e2aebe9536b36c7cad0c26f482
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2023-09-28 16:12:35 -0300

    fixed: correção de lógica da condição 'force' na função deleteFrom.

--- app/modules/files_manager.py
+++ app/modules/files_manager.py
@@ -9,13 +9,13 @@ def deleteFrom(path, ignore_types=[], force=False):
     try:
         files_to_remove = [os.path.join(path, file) for file in os.listdir(path) if os.path.splitext(file)[-1] not in ignore_types]
     except:
-        print(f'Erro de diretório:\n"{path}" não é um diretório válido. Tente novamente.')
+        print(f'Erro ao procurar arquivos. Tente novamente.')
         return False
     if len(files_to_remove) > 0:
-        if confirmMenu(f"Deseja visualizar os {len(files_to_remove)} arquivos a serem removidos?"):
-            for file in files_to_remove:
-                print(file)
         if force == False:
+            if confirmMenu(f"Deseja visualizar os {len(files_to_remove)} arquivos a serem removidos?"):
+                for file in files_to_remove:
+                    print(file)
             if confirmMenu(f"Confirma excluir todos os {len(files_to_remove)} arquivos?"):
                 for file in files_to_remove:
                     os.remove(file)
@@ -24,5 +24,6 @@ def deleteFrom(path, ignore_types=[], force=False):
         else:
             for file in files_to_remove:
                 os.remove(file)
+            print("Arquivos deletados.")
     else:
         print("Nenhum arquivo para ser deletado.")
