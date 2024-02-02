0fab89d81c1d3e221f564f8f9a619257a19ae1ce
Felipe de Sousa <felipescoelho517@gmail.com>
2024-02-02 17:54:16 -0300

    Retirada da coluna de enquadrado_simei

--- app/modules/files_manager.py
+++ app/modules/files_manager.py
@@ -8,7 +8,8 @@ def deleteFrom(path, ignore_types=[], force=False):
     print(f"Realizando limpeza em: {path}")
 
     try:
-        files_to_remove = [os.path.join(path, file) for file in os.listdir(path) if os.path.splitext(file)[-1] not in ignore_types]
+        files_to_remove = [os.path.join(path, file) for file in os.listdir(
+            path) if os.path.splitext(file)[-1] not in ignore_types]
     except:
         print(f'Erro ao procurar arquivos. Tente novamente.')
         return False
@@ -35,35 +36,37 @@ Funções de padronização para
 os arquivos CSV extraídos
 """
 
+
 def addSitacColumns(df):
     df["sitac_cft"] = ""
     df["sit_cadastro_cft"] = ""
     df["sitac_crea"] = ""
     df["sit_cadastro_crea"] = ""
-    df["enquadrado_simei"] = ""
 
 
 def formatCnpj(df):
-    cnpj14 = lambda x: x.zfill(14)
-    formated_cnpj = lambda x: f"{x[:2]}.{x[2:5]}.{x[5:8]}/{x[8:12]}-{x[12:]}"
+    def cnpj14(x): return x.zfill(14)
+    def formated_cnpj(
+        x): return f"{x[:2]}.{x[2:5]}.{x[5:8]}/{x[8:12]}-{x[12:]}"
     df.cnpj = df.cnpj.astype(str).apply(cnpj14)
     df.cnpj = df.cnpj.astype(str).apply(formated_cnpj)
 
 
 def dataInicioAtividades(df):
-    date = lambda x: f"{x[:4]}-{x[4:6]}-{x[6:8]}"
-    df.data_inicio_atividades = df.data_inicio_atividades.astype(str).apply(date)
+    def date(x): return f"{x[:4]}-{x[4:6]}-{x[6:8]}"
+    df.data_inicio_atividades = df.data_inicio_atividades.astype(
+        str).apply(date)
     df.data_inicio_atividades = pd.to_datetime(df.data_inicio_atividades)
 
 
 def formatCnaeFiscal(df):
-    cnae = lambda x: f"{x[:5]}/{x[5:]}"
+    def cnae(x): return f"{x[:5]}/{x[5:]}"
     df.cnae_fiscal = df.cnae_fiscal.astype(str).str.zfill(7)
     df.cnae_fiscal = df.cnae_fiscal.apply(cnae)
 
 
 def formatCep(df):
-    cep = lambda x: f"{x[:5]}-{x[5:]}"
+    def cep(x): return f"{x[:5]}-{x[5:]}"
     df.cep = df.cep.astype(str).str.zfill(8)
     df.cep = df.cep.apply(cep)
 
